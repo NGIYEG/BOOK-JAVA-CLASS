92ca8e33e4c0631d16499067b252e71d1b0f5eee
SCT211-0427/2023 GEORGE OTIENO <148488211+NGIYEG@users.noreply.github.com>
2024-11-03 15:31:14 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String [] args){
+        Book book1 = new Book("Betrayal In The City","Fred Igunga");
+        System.out.println(book1.getDetails());
+    }
+}
